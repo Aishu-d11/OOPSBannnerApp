5dee2a26194467075c96d96cbfefccf1781cc0f7
D. Aishvarya <dd3048@srmist.edu.in>
2026-02-24 08:02:01 +0530

    added UC2 and UC3 implementations using string.join()

---
+++ OOPSBannnerApp.java
@@ -0,0 +1,13 @@
+public class OOPSBannnerApp {
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   ******   ***** ");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *     *   *     ");
+        System.out.println("*     * *     * ******     ***  ");
+        System.out.println("*     * *     * *             * ");
+        System.out.println("*     * *     * *             * ");
+        System.out.println(" *****   *****  *        *****  ");
+
+    }
+}
